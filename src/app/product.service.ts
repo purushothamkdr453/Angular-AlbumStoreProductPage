9226ff4f6e92a020fc3142c01fd6957985691cc1
purushothamkdr453 <purushothamkdr453@gmail.com>
2018-08-14 16:44:05 +0530

    4
    4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response }  from '@angular/http';
-import { map } from 'rxjs/operators';
+import  'rxjs/add/operators/map';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +11,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-  return this._http.get(this._albumUrl).pipe(map((response)=>response.json()));
+  return this._http.get(this._albumUrl).map((response)=>response.json());
   }
 }
